3229492e05f719ea5081607fc40c307c7b5077f4
Elias Reynaga <82005746+Elias-26@users.noreply.github.com>
2023-04-17 14:17:25 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,91 +1,162 @@
-const inputTexto = document.getElementById('input-texto');
-const btnEncriptar = document.getElementById('btn-encriptar');
-const btnDesencriptar = document.getElementById('btn-desencriptar');
-const inputResultado = document.getElementById('mensaje-texto');
-const btnCopiar = document.getElementById('btn-copy');
-const soloLetras ='^[a-z !ñ]+$';
+// input
+let textarea = document.getElementById("txt-input");
+let informacion = document.querySelector(".txt-instrucciones");
 
-document.addEventListener('DOMContentLoaded', () => {
-  btnEncriptar.addEventListener('click', encriptarTexto);
-  btnDesencriptar.addEventListener('click', desencriptarTexto);
-  btnCopiar.addEventListener('click', copiarTexto);
-});
+// contenedor Ilustracion
+const contenedorInicial = document.querySelector(".ilustracion");
 
-function encriptarTexto(e) {
-  e.preventDefault();
-  inputResultado.value = '';
-  let texto = inputTexto.value;
-  
-  if(texto.match(soloLetras)!=null){
-    let palabras = texto.split(' ');
-    let nuevasPalabras = [];
-    
-    for (let palabra of palabras) {
-      palabra = palabra.replaceAll('e','enter');
-      palabra = palabra.replaceAll('i','imes');
-      palabra = palabra.replaceAll('a','ai');
-      palabra = palabra.replaceAll('o','ober');
-      palabra = palabra.replaceAll('u','ufat');      
-      
-      nuevasPalabras.push(palabra);    
-    }
+// output
+let secRespuesta = document.querySelector(".sec-respuesta");
+let textareaOut = document.getElementById("txt-output");
+console.log(textareaOut);
+let contenedorSalida = document.getElementById("contenedor-salida");
 
-    const resultado = nuevasPalabras.join(' ');
-    
-    inputResultado.value = resultado;
-  } else {
-    mostrarError('Solo se permiten letras minúsculas, sin acentos');
-    return;
-  }  
+// btn
+const btnEncriptar = document.querySelector(".btn-encriptar");
+const btnDesencriptar = document.querySelector(".btn-desencriptar");
+const btnCopiar = document.getElementById("btn-copiar");
+
+// verificaciones
+var mayus = /[A-Z]/gm;
+var caracteres = /[~!@#$%^&*()_+|}{[\]\\\/?><:"`;.,áéíóúÁÉÍÓÚ'1-9]/gm;
+
+// limpiar contenedor de salida
+function quitarImg() {
+  let mensajeEntrada = textarea.value;
+  if (mensajeEntrada.length > 0) {
+    contenedorInicial.style.display = "none";
+  } else contenedorInicial.style.display = "";
 }
 
-function desencriptarTexto(e) {
-  e.preventDefault();  
-  inputResultado.value = '';
-  let texto = inputTexto.value;
-
-  if(texto.match(soloLetras)!=null){
-    let palabras = texto.split(" ");
-    let nuevasPalabras = [];    
-
-    for (let palabra of palabras) {
-      palabra = palabra.replaceAll('enter','e');
-      palabra = palabra.replaceAll('imes','i');
-      palabra = palabra.replaceAll('ai','a');
-      palabra = palabra.replaceAll('ober','o');
-      palabra = palabra.replaceAll('ufat','u');
-      nuevasPalabras.push(palabra);    
-    }
-
-    const resultado = nuevasPalabras.join(' ');
-    
-    inputResultado.value = resultado;
-  } else {
-    mostrarError('Solo se permiten letras minúsculas, sin acentos');
-    return;
-  }  
+// foco en el textarea de entrada
+function foco() {
+  textarea.focus();
 }
 
-function mostrarError(mensaje) {
-  const existeError = document.querySelector('.error');
-  
-  if(!existeError) {
-      const formulario = document.getElementById('formulario');
-      const divMensaje = document.createElement('div');
-      divMensaje.classList.add('error');
-  
-      divMensaje.textContent = mensaje;            
-      formulario.appendChild(divMensaje);
-      
-      setTimeout(()=> {
-          divMensaje.remove();
-      }, 3000);
+// encriptar mensaje
+function encriptar() {
+  if (validarMayusculas() || validarCaracter() || validarCampo()) {
+    Swal.fire({
+      icon: "error",
+      title: "Lo siento",
+      text: "No cumple con las condiciones",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  } else if (!validarMayusculas() && !validarCampo() && !validarCaracter()) {
+    let nuevoTexto = textarea.value;
+    nuevoTexto = nuevoTexto
+      .replace(/e/gm, "enter")
+      .replace(/i/gm, "imes")
+      .replace(/a/gm, "ai")
+      .replace(/o/gm, "ober")
+      .replace(/u/gm, "ufat");
+    secRespuesta.classList.add("sec-ed");
+    contenedorSalida.classList.add("respuesta-ed");
+    btnCopiar.style.display = "block";
+    textareaOut.style.display = "block";
+    textareaOut.value = nuevoTexto;
+    quitarImg();
   }
 }
 
-function copiarTexto(e) {
-    e.preventDefault(); 
-    const mensaje = inputResultado.value;
-
-    navigator.clipboard.writeText(mensaje);
+// desencriptar mensaje
+function desencriptar() {
+  if (validarMayusculas() || validarCaracter() || validarCampo()) {
+    Swal.fire({
+      icon: "error",
+      title: "Lo siento",
+      text: "No cumple con las condiciones",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  } else if (!validarMayusculas() && !validarCampo() && !validarCaracter()) {
+    let nuevoTexto = textarea.value;
+    nuevoTexto = nuevoTexto
+      .replace(/enter/gm, "e")
+      .replace(/imes/gm, "i")
+      .replace(/ai/gm, "a")
+      .replace(/ober/gm, "o")
+      .replace(/ufat/gm, "u");
+    secRespuesta.classList.add("sec-ed");
+    contenedorSalida.classList.add("respuesta-ed");
+    btnCopiar.style.display = "block";
+    textareaOut.style.display = "block";
+    textareaOut.value = nuevoTexto;
+    quitarImg();
+  }
 }
+
+// copiar mensaje
+
+function copiar() {
+  let copiado = textareaOut.value;
+  textarea.value = "";
+  textareaOut.style.display = "none";
+  btnCopiar.style.display = "none";
+  secRespuesta.classList.remove("sec-ed");
+  contenedorSalida.classList.remove("respuesta-ed");
+  contenedorInicial.style.display = "";
+  foco();
+  navigator.clipboard.writeText(copiado).then(() => {
+    btnCopiar.textContent = "Copiado ✅";
+    window.setTimeout(() => {
+      btnCopiar.textContent = "Copiar";
+      btnCopiar.classList.remove("btn-copiado");
+    }, 1000);
+  });
+  Swal.fire({
+    icon: "success",
+    title: "Copiado",
+    showConfirmButton: false,
+    timer: 1500,
+  });
+  
+}
+
+// verificar si existen mayusculas
+function validarMayusculas() {
+  let validarMayusculas = mayus.test(textarea.value);
+  if (validarMayusculas) {
+    textarea.style.color = "#ff0000";
+    informacion.style.color = "#ff0000";
+    foco();
+    return true;
+  } else {
+    textarea.style.color = "var(--d-blue-300)";
+    informacion.style.color = "var(--gray-500)";
+    return false;
+  }
+}
+
+// verificar si existen caracteres especiales
+function validarCaracter() {
+  let validarCaracteres = caracteres.test(textarea.value);
+  if (validarCaracteres) {
+    textarea.style.color = "#ff0000";
+    informacion.style.color = "#ff0000";
+    foco();
+    return true;
+  } else {
+    textarea.style.color = "var(--d-blue-300)";
+    informacion.style.color = "var(--gray-500)";
+    return false;
+  }
+}
+
+// verificar que el campo no esté vacío
+function validarCampo() {
+  if (textarea.value.length == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+foco();
+
+btnEncriptar.addEventListener("click", encriptar);
+
+btnDesencriptar.addEventListener("click", desencriptar);
+
+btnCopiar.addEventListener("click", copiar);
